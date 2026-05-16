31a970524276f139666b833d53f454eff738e47f
HopzAlot <rehansaqib2006@gmail.com>
2026-05-16 17:09:36 +0500

    Added models for User,Job and Application.

---
+++ backend/models/Application.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema({
+    jobId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job"
+    },
+    name: String,
+    email: String,
+    resumeLink: String,
+    appliedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Application", applicationSchema);
---
+++ backend/models/Job.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    salary: Number,
+    location: String,
+    type: String,
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Job", jobSchema);
